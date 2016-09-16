3ff12ab94a7d163acf984403cbd65e862ab223ac
Evan Debenham <Evan.SHPX@gmail.com>
2016-09-16 02:18:51 -0400

    v0.4.2b: updated version info

--- desktop/src/com/watabou/pd/desktop/DesktopLauncher.java
+++ desktop/src/com/watabou/pd/desktop/DesktopLauncher.java
@@ -36,14 +36,14 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		String version = DesktopLauncher.class.getPackage().getSpecificationVersion();
 		if (version == null) {
-			version = "0.4.1a";
+			version = "0.4.2b";
 		}
 
 		int versionCode;
 		try {
 			versionCode = Integer.parseInt(DesktopLauncher.class.getPackage().getImplementationVersion());
 		} catch (NumberFormatException e) {
-			versionCode = 118;
+			versionCode = 129;
 		}
 
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
