f051fe0d5a1b219a0eff6aab1c2f85dbe654278b
Evan Debenham <Evan@ShatteredPixel.com>
2019-10-17 14:06:37 -0400

    ported over 0.7.5c changes, updated version info

--- desktop/src/com/watabou/pd/desktop/DesktopLauncher.java
+++ desktop/src/com/watabou/pd/desktop/DesktopLauncher.java
@@ -34,14 +34,14 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		String version = DesktopLauncher.class.getPackage().getSpecificationVersion();
 		if (version == null) {
-			version = "0.7.5b";
+			version = "0.7.5c";
 		}
 
 		int versionCode;
 		try {
 			versionCode = Integer.parseInt(DesktopLauncher.class.getPackage().getImplementationVersion());
 		} catch (NumberFormatException e) {
-			versionCode = 378;
+			versionCode = 379;
 		}
 
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
