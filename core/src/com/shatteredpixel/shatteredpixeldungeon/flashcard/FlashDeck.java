925a4458c32d7644e91ecfe6d623552dc87ce5f4
AndersenJ <johnathan.andersen@byu.edu>
2019-11-02 20:48:38 -0600

    Added some functionality to FlashDeck, including a static method for generating test decks

--- core/src/com/shatteredpixel/shatteredpixeldungeon/flashcard/FlashDeck.java
+++ core/src/com/shatteredpixel/shatteredpixeldungeon/flashcard/FlashDeck.java
@@ -1,7 +1,6 @@
 package com.shatteredpixel.shatteredpixeldungeon.flashcard;
 
 import java.util.Vector;
-//import java.util.Random;
 import com.watabou.utils.Random;
 
 public class FlashDeck implements IFlashDeck
@@ -10,6 +9,22 @@ public class FlashDeck implements IFlashDeck
 	public FlashDeck(Vector<FlashQuestion> questions)
 	{
 		this.questions = questions;
+	}
+
+	public static FlashDeck getTestDeck() {
+		Vector<FlashQuestion> multQuestions = new Vector<FlashQuestion>();
+		for (int i = 0; i < 13; i++) {
+			for (int j = 0; j < 13; j++) {
+				String q = Integer.toString(i) + " * " + Integer.toString(j);
+				String a = Integer.toString(i*j);
+				multQuestions.add(new FlashQuestion(q, a));
+			}
+		}
+		return new FlashDeck(multQuestions);
+	}
+
+	public void addQuestion(FlashQuestion newQuestion) {
+		questions.add(newQuestion);
 	}
 
 	//this function could definitely be made faster if it ends up slowing things down
--- core/src/com/shatteredpixel/shatteredpixeldungeon/flashcard/IFlashDeck.java
+++ core/src/com/shatteredpixel/shatteredpixeldungeon/flashcard/IFlashDeck.java
@@ -3,4 +3,5 @@ package com.shatteredpixel.shatteredpixeldungeon.flashcard;
 public interface IFlashDeck
 {
 	IFlashQuestion getQuestion();
+	void addQuestion(FlashQuestion newQuestion);
 }
